285e9f4e1b0d8671f57c2bdfa0997451bf12538f
martin <martin@greatfire.org>
2014-11-24 09:30:20 +0800

    translatable, better build script

--- build_demo.sh
+++ build_demo.sh
@@ -2,12 +2,19 @@
 SRC_DIR="src"
 DEMO_DIR="demo"
 
-RWB_EMAIL="rwb@greatfire.org"
-RWB_JQUERY_URL="jquery-1.11.1.min.js"
-RWB_REDIRECTION_URLS="https://bbc1.azurewebsites.net https://github.com/greatfire/wiki"
-RWB_TITLE="Redirect When Blocked"
+declare -A VARS
+VARS[RWB_EMAIL]="rwb@greatfire.org"
 
-RWB_VERSION=$(date)
+VARS[RWB_JQUERY_URL]="jquery-1.11.1.min.js"
+VARS[RWB_REDIRECTION_URLS]="https://bbc1.azurewebsites.net https://github.com/greatfire/wiki"
+VARS[RWB_TITLE]="Redirect When Blocked"
+
+VARS[RWB_FALLBACK_INTRO]="This website could not be accessed. Alternative URLs:"
+VARS[RWB_FALLBACK_THIS_URL]="This URL"
+VARS[RWB_FALLBACK_EMAIL]="Or contact us by sending an email to"
+VARS[RWB_FALLBACK_REDIRECTING]="redirecting"
+
+VARS[RWB_VERSION]=$(date)
 
 while read FILE; do
 	
@@ -19,18 +26,19 @@ while read FILE; do
 			
 			if [[ $LINE =~ .*\{RWB_REDIRECTION_URL\}.* ]]; then
 				
-				for RWB_REDIRECTION_URL in $RWB_REDIRECTION_URLS; do
+				for RWB_REDIRECTION_URL in ${VARS[RWB_REDIRECTION_URLS]}; do
 					echo "$LINE" | sed "s,{RWB_REDIRECTION_URL},$RWB_REDIRECTION_URL,g"
 				done
 			else
 				echo "$LINE"
 			fi
 			
-		done < $SRC_DIR/$FILE \
-		| sed "s,{RWB_EMAIL},$RWB_EMAIL,g" \
-		| sed "s,{RWB_JQUERY_URL},$RWB_JQUERY_URL,g" \
-		| sed "s,{RWB_TITLE},$RWB_TITLE,g" \
-		| sed "s,{RWB_VERSION},$RWB_VERSION,g" \
+		done < $SRC_DIR/$FILE | while read LINE; do
+			for KEY in "${!VARS[@]}"; do 
+				LINE=$(echo "$LINE" | sed "s,{$KEY},${VARS[$KEY]},g")
+			done
+			echo "$LINE"
+		done \
 		 > $DEMO_FILE
 		
 	else
